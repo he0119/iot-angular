2c2738e1673d84fe5e6db4fe8d5bfd88a9bf9143
hemengyang <hmy0119@gmail.com>
2019-08-18 01:07:31 +0800

    Add datepipes in status page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,13 @@ import { RefreshTokenInterceptor } from './_helpers/refresh-token-interceptor';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
+// i18n
+import { registerLocaleData } from '@angular/common';
+import localeZh from '@angular/common/locales/zh';
+
+// the second parameter 'zh' is optional
+registerLocaleData(localeZh, 'zh');
+
 @NgModule({
   declarations: [
     AppComponent,
